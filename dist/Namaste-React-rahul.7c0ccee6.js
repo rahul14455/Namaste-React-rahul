895d6cd9ef22466729034e34c9e3c020b08acf92
rahul14455 <42345755+rahul14455@users.noreply.github.com>
2025-08-02 15:20:00 +0530

    third-commit

---
+++ dist/Namaste-React-rahul.7c0ccee6.js
@@ -0,0 +1,25 @@
+const head = React.createElement("h1", {
+    id: "heading",
+    xyz: "abc"
+}, "Hello world from React !");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(head);
+const parent = React.createElement("h1", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child"
+    }, [
+        React.createElement("h1", {}, "This is H1 Tag"),
+        React.createElement("h2", {}, "This is H2 tag")
+    ])
+], React.createElement("div", {
+    id: "child2"
+}, [
+    React.createElement("h1", {}, "This is H1 Tag"),
+    React.createElement("h2", {}, "This is H2 tag")
+]));
+console.log(parent);
+root.render(parent);
+
+//# sourceMappingURL=Namaste-React-rahul.7c0ccee6.js.map
